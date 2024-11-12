16eaef52385092c9c5c62d815cf14b52a205c807
Ujjwal Dhami <148849457+ujjwaldhami@users.noreply.github.com>
2024-11-12 21:21:53 +0530

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,155 @@
+from tkinter import *
+
+class Bill_App:
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("1350x700+0+0")
+        self.root.title("Billing Software")
+        bg_color = "#074463"
+        title = Label(self.root , text = "Billing Software",bd= 12,relief = GROOVE,bg=bg_color,fg= "white",font = ("times new roman",30,"bold"),pady = 2).pack(fill=X)
+
+        self.soap = IntVar()
+        self.face_cream = IntVar()
+        self.face_wash = IntVar()
+        self.spray = IntVar()
+        self.gell = IntVar()
+        self.lotion = IntVar()
+
+        self.rice = IntVar()
+        self.food_oil = IntVar()
+        self.daal = IntVar()
+        self.wheat = IntVar()
+        self.sugar = IntVar()
+        self.tea_leaves = IntVar()
+
+        self.tea = IntVar()
+        self.coffee = IntVar()
+        self.cold_drink = IntVar()
+        self.milk = IntVar()
+        self.lassi = IntVar()
+        self.juice = IntVar()
+
+        F1= LabelFrame(self.root, bd=10,relief=GROOVE,text="Customer Details", font=("times new roman",15,"bold"),fg="gold",bg=bg_color)
+        F1.place(x=0,y=80,relwidth=1)
+
+        cname_label= Label(F1,text="Customer Name",bg=bg_color,fg="white",font=("times new roman",18,"bold")).grid(row=0,column=0,padx=20,pady=5)
+        cname_txt = Entry(F1,width=15,font="arial 15",bd=7,relief=SUNKEN).grid(row=0,column=1,padx=10,pady=5)
+
+        cphone_label= Label(F1,text="Phone Number",bg=bg_color,fg="white",font=("times new roman",18,"bold")).grid(row=0,column=2,padx=20,pady=5)
+        cphone_txt = Entry(F1,width=15,font="arial 15",bd=7,relief=SUNKEN).grid(row=0,column=3,padx=10,pady=5)
+
+        c_bill_label= Label(F1,text="Bill Number",bg=bg_color,fg="white",font=("times new roman",18,"bold")).grid(row=0,column=4,padx=20,pady=5)
+        c_bill_txt = Entry(F1,width=15,font="arial 15",bd=7,relief=SUNKEN).grid(row=0,column=5,padx=10,pady=5)
+
+        bill_btn = Button(F1,text="Search",width=10,bd=7,font="arial 12 bold").grid(row=0,column=6,pady=10,padx=15)
+
+        F2 = LabelFrame(self.root,bd=10,relief=GROOVE,text="Cosmetics", font=("times new roman",15,"bold"),fg="gold",bg=bg_color)
+        F2.place(x=5,y=180,width=325,height=380)
+
+        bath_lbl = Label(F2,text="Bath soap",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=0,column=0,padx=10,pady=10,sticky="w")
+        bath_txt=Entry(F2,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=0,column=1,padx=10,pady=10)
+
+        face_cream_lbl = Label(F2,text="Face Cream",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=1,column=0,padx=10,pady=10,sticky="w")
+        face_cream_txt=Entry(F2,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=1,column=1,padx=10,pady=10)
+
+        face_w_lbl = Label(F2,text="Face Wash",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=2,column=0,padx=10,pady=10,sticky="w")
+        face_w_txt=Entry(F2,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=2,column=1,padx=10,pady=10)
+
+        hair_s_lbl = Label(F2,text="Hair Shampoo",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=3,column=0,padx=10,pady=10,sticky="w")
+        hair_s_txt=Entry(F2,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=3,column=1,padx=10,pady=10)
+
+
+        hair_g_lbl = Label(F2,text="Hair Gel",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=4,column=0,padx=10,pady=10,sticky="w")
+        hair_g_txt=Entry(F2,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=4,column=1,padx=10,pady=10)
+
+        body_lbl = Label(F2,text="Body Lotion",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=5,column=0,padx=10,pady=10,sticky="w")
+        body_txt=Entry(F2,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=5,column=1,padx=10,pady=10)
+
+        F3 = LabelFrame(self.root,bd=10,relief=GROOVE,text="Groceries", font=("times new roman",15,"bold"),fg="gold",bg=bg_color)
+        F3.place(x=325,y=180,width=325,height=380)
+
+        rice_lbl = Label(F3,text="Rice",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=0,column=0,padx=10,pady=10,sticky="w")
+        rice_txt=Entry(F3,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=0,column=1,padx=10,pady=10)
+
+        oil_cream_lbl = Label(F3,text="Food Oil",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=1,column=0,padx=10,pady=10,sticky="w")
+        oil_cream_txt=Entry(F3,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=1,column=1,padx=10,pady=10)
+
+        daal_lbl = Label(F3,text="Daal",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=2,column=0,padx=10,pady=10,sticky="w")
+        daal_txt=Entry(F3,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=2,column=1,padx=10,pady=10)
+
+        wheat_lbl = Label(F3,text="Wheat",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=3,column=0,padx=10,pady=10,sticky="w")
+        wheat_txt=Entry(F3,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=3,column=1,padx=10,pady=10)
+
+        sugar_lbl = Label(F3,text="Sugar",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=4,column=0,padx=10,pady=10,sticky="w")
+        sugar_txt=Entry(F3,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=4,column=1,padx=10,pady=10)
+
+        tea_lbl = Label(F3,text="Tea Leaves",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=5,column=0,padx=10,pady=10,sticky="w")
+        tea_txt=Entry(F3,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=5,column=1,padx=10,pady=10)
+
+        F4 = LabelFrame(self.root,bd=10,relief=GROOVE,text="Drinks", font=("times new roman",15,"bold"),fg="gold",bg=bg_color)
+        F4.place(x=644,y=180,width=325,height=380)
+
+        tea_lbl = Label(F4,text="Tea",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=0,column=0,padx=10,pady=10,sticky="w")
+        tea_txt=Entry(F4,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=0,column=1,padx=10,pady=10)
+
+        coffee_lbl = Label(F4,text="Coffee",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=1,column=0,padx=10,pady=10,sticky="w")
+        coffee_cream_txt=Entry(F4,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=1,column=1,padx=10,pady=10)
+
+        cold_drink_lbl = Label(F4,text="Cold Drink",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=2,column=0,padx=10,pady=10,sticky="w")
+        cold_drink_txt=Entry(F4,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=2,column=1,padx=10,pady=10)
+
+        milk_lbl = Label(F4,text="Milk",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=3,column=0,padx=10,pady=10,sticky="w")
+        milk_txt=Entry(F4,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=3,column=1,padx=10,pady=10)
+
+
+        lassi_lbl = Label(F4,text="Lassi",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=4,column=0,padx=10,pady=10,sticky="w")
+        lassi_txt=Entry(F4,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=4,column=1,padx=10,pady=10)
+
+        juice_lbl = Label(F4,text="Juice",font=("times new roman",16,"bold"),bg=bg_color,fg="lightgreen").grid(row=5,column=0,padx=10,pady=10,sticky="w")
+        juice_txt=Entry(F4,width=10,font=("times new roman",16,"bold"),bd=5,relief=SUNKEN).grid(row=5,column=1,padx=10,pady=10)
+
+
+        F5 =Frame(self.root,bd=10,relief=GROOVE)
+        F5.place(x=990,y=180,width=340,height=380)
+
+        bill_title = Label(F5,text="Bill Area",font="arial 15 bold",bd=7,relief=GROOVE).pack(fill=X)
+        scroll_y = Scrollbar(F5,orient=VERTICAL)
+        self.textarea=Text(F5,yscrollcommand=scroll_y.set)
+        scroll_y.pack(side=RIGHT,fill=Y)
+        scroll_y.config(command=self.textarea.yview)
+        self.textarea.pack(fill=BOTH,expand=1)
+
+
+        F6=LabelFrame(self.root,bd=10,relief=GROOVE,text="Bill Menu", font=("times new roman",15,"bold"),fg="gold",bg=bg_color)
+        F6.place(x=0,y=560,relwidth=1,height=140)
+
+        m1_lbl=Label(F6,text="Total Cosmetic price",bg=bg_color,fg="white",font=("times new roman",14,"bold")).grid(row=0,column=0,padx=20,pady=1,sticky="w")
+        m1_text=Entry(F6,width=18,font="arial 10 bold",bd=7,relief=SUNKEN).grid(row=0,column=1,padx=5,pady=1)
+
+        m2_lbl=Label(F6,text="Total Grocery price",bg=bg_color,fg="white",font=("times new roman",14,"bold")).grid(row=1,column=0,padx=20,pady=1,sticky="w")
+        m2_text=Entry(F6,width=18,font="arial 10 bold",bd=7,relief=SUNKEN).grid(row=1,column=1,padx=5,pady=1)
+
+        m3_lbl=Label(F6,text="Total Cold Drinks price",bg=bg_color,fg="white",font=("times new roman",14,"bold")).grid(row=2,column=0,padx=20,pady=1,sticky="w")
+        m3_text=Entry(F6,width=18,font="arial 10 bold",bd=7,relief=SUNKEN).grid(row=2,column=1,padx=5,pady=1)
+
+        c1_lbl=Label(F6,text= "Cosmetic Tax",bg=bg_color,fg="white",font=("times new roman",14,"bold")).grid(row=0,column=2,padx=20,pady=1,sticky="w")
+        c1_text=Entry(F6,width=18,font="arial 10 bold",bd=7,relief=SUNKEN).grid(row=0,column=3,padx=5,pady=1)
+
+        c2_lbl=Label(F6,text="Grocery Tax",bg=bg_color,fg="white",font=("times new roman",14,"bold")).grid(row=1,column=2,padx=20,pady=1,sticky="w")
+        c2_text=Entry(F6,width=18,font="arial 10 bold",bd=7,relief=SUNKEN).grid(row=1,column=3,padx=5,pady=1)
+
+        c3_lbl=Label(F6,text="Cold Drinks Tax",bg=bg_color,fg="white",font=("times new roman",14,"bold")).grid(row=2,column=2,padx=20,pady=1,sticky="w")
+        c3_text=Entry(F6,width=18,font="arial 10 bold",bd=7,relief=SUNKEN).grid(row=2,column=3,padx=5,pady=1)
+
+        btn_F=Frame(F6,bd=7,relief=GROOVE,bg=bg_color)
+        btn_F.place(x=750,width=580,height=105)
+
+        total_btn=Button(btn_F,text="Total",bg="grey",font="arial 12 bold",bd=5,width=11,fg="black",pady=15).grid(row=0,column=0,padx=8,pady=5)
+        Gbill_btn=Button(btn_F,text="Generate bill",bg="grey",font="arial 12 bold",bd=5,width=11,fg="black",pady=15).grid(row=0,column=1,padx=5,pady=5)
+        Clear_btn=Button(btn_F,text="Clear",bg="grey",font="arial 12 bold",bd=5,width=11,fg="black",pady=15).grid(row=0,column=2,padx=5,pady=5)
+        Exit_btn=Button(btn_F,text="Exit",bg="grey",font="arial 12 bold",bd=5,width=11,fg="black",pady=15).grid(row=0,column=3,padx=5,pady=5)
+
+root = Tk()
+obj = Bill_App(root)
+
+root.mainloop()
